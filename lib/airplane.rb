2801da045422e71949b4a5e8f50a5cba047a152d
rovaira <rovaira@gmail.com>
2015-06-03 22:18:12 -0400

    Functioning program, refactored using 'let' syntax

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,2 +1,51 @@
 class Airplane
+
+  def initialize(type, wingloading, horsepower)
+    @type = type
+    @wingloading = wingloading
+    @horsepower = horsepower
+    @engine = false
+    @flying = false
+  end
+
+  def type
+    @type
+  end
+
+  def wingloading
+    @wingloading
+  end
+
+  def horsepower
+    @horsepower
+  end
+
+  def start
+    if @engine == false
+      @engine = true
+      "my_plane started"
+    else
+      "my_plane already started"
+    end
+  end
+
+  def takeoff
+    if @engine == false
+      "airplane not started, please start"
+    else
+      @flying = true
+      "airplane launched"
+    end
+  end
+
+  def land
+    if @engine == false
+      "airplane not started, please start"
+    elsif @flying == false
+      "airplane already on the ground"
+    elsif @flying == true
+      @flying = false
+      "airplane landed"
+    end
+  end
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,15 +1,69 @@
 require_relative "../../lib/airplane"
 
-describe Airplane do
-  describe "#initialization" do
+RSpec.describe Airplane do
+  let (:my_plane) { Airplane.new("cesna", 10, 150) }
+  it 'has a type' do
+    expect(my_plane.type).to eq("cesna")
   end
 
-  describe "#land" do
+  it 'has wingloading' do
+    expect(my_plane.wingloading).to eq(10)
   end
 
-  describe "#takeoff" do
+  it 'has horsepower' do
+    expect(my_plane.horsepower).to eq(150)
   end
 
   describe "#start" do
+    context 'if the plane is turned off' do
+      it 'returns engine was just started' do
+        expect(my_plane.start).to eq("my_plane started")
+      end
+    end
+
+    context 'if the plane is already on' do
+      it 'returns engine has already been running' do
+        my_plane.start
+        expect(my_plane.start).to eq("my_plane already started")
+      end
+    end
+  end
+
+  describe "#takeoff" do
+    context 'if the engine is not running' do
+      it 'returns airplane not started, please start' do
+        expect(my_plane.takeoff).to eq('airplane not started, please start')
+      end
+    end
+
+    context 'if the plane was already started' do
+      it 'returns airplane launched' do
+        my_plane.start
+        expect(my_plane.takeoff).to eq('airplane launched')
+      end
+    end
+  end
+
+  describe "#land" do
+    context 'plane is currently flying' do
+      it 'returns plane has successfully landed' do
+        my_plane.start
+        my_plane.takeoff
+        expect(my_plane.land).to eq('airplane landed')
+      end
+    end
+
+    context 'plane is not started' do
+      it 'returns turn on the plane' do
+        expect(my_plane.land).to eq('airplane not started, please start')
+      end
+    end
+
+    context 'plane already on the ground' do
+      it 'returns airplane already on the ground' do
+        my_plane.start
+        expect(my_plane.land).to eq('airplane already on the ground')
+      end
+    end
   end
 end
